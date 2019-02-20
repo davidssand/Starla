a7f2f3e7b82ef8fa5f9732354ac5720b07248367
David Steiner Sand <davidssand@hotmail.com>
2019-02-20 19:43:15 -0300

    Adapted camera module to import
    Camera now inherites PiCamera

--- Starla/Actuators/Camera.py
+++ Starla/Actuators/Camera.py
@@ -1,23 +1,20 @@
 import picamera     # Importing the library for camera module
 import time
 
-objCamera = picamera.PiCamera()
-
-
-class Camera():
-    def __init__(self, vPixels, hPixels, framerate):
-        #super().__init__()
-        objCamera.resolution = (vPixels, hPixels)
-        objCamera.framerate = framerate
-        objCamera.vflip = 180
-        objCamera.hflip = 180
+class Camera(picamera.PiCamera):
+    def __init__(self):
+        super().__init__(resolution = (1920, 1080), framerate = 30)
+        self.vflip = 180
+        self.hflip = 180
 
     def startRecording(self):
-        objCamera.start_recording('/home/pi/Components/Camera/video.h264')
+        self.start_recording('/home/pi/Starla/CameraData//video.h264')
 
     def stopRecording(self):
-        objCamera.stop_recording()
+        self.stop_recording()
 
     def takePicture(self):
-        objCamera.capture('/home/pi/Components/Camera/imag.jpg')
+        self.capture('/home/pi/Starla/CameraData/imag.jpg')
+
+
 
