e6fa4579ea59d6b274913be0df2926f87f702318
David Steiner Sand <davidssand@hotmail.com>
2019-02-20 19:44:37 -0300

    Adding test_ module for sensors
    Abolishing old "run" module

--- Starla/Sensors/test_gps.py
+++
@@ -1,6 +0,0 @@
-import sys
-sys.path.append("/home/pi/Starla")
-
-from Sensor.GPS import GPS
-
-gps = GPS()
--- Starla/Sensors/test_sensors.py
+++ Starla/Sensors/test_sensors.py
@@ -3,28 +3,32 @@ import serial
 
 import sys
 sys.path.append("/home/pi/Starla")
+from Sensors.GPS import GPS
 from Sensors.MPU6050 import MPU6050
 from Sensors.BME280 import BME280
 
-class TestSensors:
+class Tester:
     def __init__(self):
         self.bme280 = BME280()
         self.mpu6050 = MPU6050()
-        self.t = 0
-
-    def show_sensors_pack(self, delay):
+        self.gps = GPS()
         self.t = time.time()
-        while 1:
-            msg = "$"
-            msg += str(round(time.time() - self.t, 2)) + ","
-            c = ""
-            for i in range(len(self.mpu6050.dataPackage())):
-                msg += c + str(self.mpu6050.dataPackage()[i])
-                c = ","
-            for i in range(len(self.bme280.dataPackage())):
-                msg += c + str(self.bme280.dataPackage()[i])
-                c = ","
-            msg += "#"
-            print(msg + "\n")
-            msg = ""
-            time.sleep(delay)
+    
+    def test_sensor(self, sensor):
+        print("Testing " + str(sensor))
+        msg = ""
+        c = ""
+        for i in range(len(sensor.dataPackage())):
+            msg += c + str(sensor.dataPackage()[i])
+            c = ","
+        return msg
+
+    def test_sensors_pack(self):
+        msg = "$"
+        msg += str(round(time.time() - self.t, 2)) + ","
+        msg += self.test_sensor(self.mpu6050)
+        msg += self.test_sensor(self.bme280)
+        msg += self.test_sensor(self.gps)
+        msg += "#\n"
+        return msg
+
